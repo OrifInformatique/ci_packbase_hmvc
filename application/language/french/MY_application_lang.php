d95d6db68fcc12bfb53700ec1c0d3cf44e4a7787
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2019-08-15 10:37:15 +0200

    CodeIngiter normal installation

---
+++ application/language/french/MY_application_lang.php
@@ -0,0 +1,31 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * French translations of applcation's texts
+ *
+ * @author      Didier Viret
+ * @link        https://github.com/OrifInformatique/ci_pack_base
+ * @copyright   Copyright (c), Orif <http://www.orif.ch>
+ */
+
+// Application name
+$lang['app_title']                      = 'Section informatique<br />Package de base';
+
+// Page titles
+$lang['page_prefix']                    = 'Pack base';
+
+// Date and time formats
+$lang['date_format_short']              = 'd.m.Y';
+$lang['datetime_format_short']          = 'd.m.Y H:i';
+
+// Fields labels
+
+// List headers
+
+// Buttons
+$lang['btn_login']                      = 'Se connecter';
+$lang['btn_logout']                     = 'Se déconnecter';
+$lang['btn_admin']                      = 'Administration';
+
+// Messages
+
+// Other texts
