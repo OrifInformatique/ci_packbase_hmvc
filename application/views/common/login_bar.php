1488d9ca2c514d22dd52178ba5eea8fcd8405fa1
Didier Viret <didier.viret@orif.ch>
2019-11-27 13:23:37 +0100

    Replace standard bootstrap with Orif bootstrap css. New orif logo.

--- application/views/common/login_bar.php
+++ application/views/common/login_bar.php
@@ -1,27 +1,27 @@
 <div class="container" >
   <div class="row xs-center">
-    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 ">
-      <a href="<?php echo base_url(); ?>" ><img src="<?php echo base_url("assets/images/logo.jpg"); ?>" ></a>
+    <div class="col-sm-3">
+      <a href="<?php echo base_url(); ?>" ><img src="<?php echo base_url("assets/images/logo.png"); ?>" ></a>
     </div>
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-      <a href="<?php echo base_url(); ?>" ><h1><?php echo $this->lang->line('app_title'); ?></h1></a>
+    <div class="col-sm-6">
+      <a href="<?php echo base_url(); ?>" class="text-info"><h1><?php echo lang('app_title'); ?></h1></a>
     </div>
-    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12" >
-      <div class="nav nav-pills" style="margin-top:20px;">
+    <div class="col-sm-3" >
+      <div class="nav nav-pills" >
         <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) { ?>
           
           <!-- ADMIN ACCESS ONLY -->
           <?php if ($_SESSION['user_access'] >= ACCESS_LVL_ADMIN) { ?>
-              <a href="<?php echo base_url("admin/"); ?>" ><?php echo $this->lang->line('btn_admin'); ?></a><br />
+              <a href="<?php echo base_url("admin/"); ?>" ><?php echo lang('btn_admin'); ?></a><br />
           <?php } ?>
           <!-- END OF ADMIN ACCESS -->
 
           <!-- Logged in, display a "logout" button -->
-          <a href="<?php echo base_url("auth/auth/logout"); ?>" ><?php echo $this->lang->line('btn_logout'); ?></a>
+          <a href="<?php echo base_url("auth/logout"); ?>" ><?php echo lang('btn_logout'); ?></a>
 
         <?php } else { ?>
           <!-- Not logged in, display a "login" button -->
-          <a href="<?php echo base_url("auth"); ?>" ><?php echo $this->lang->line('btn_login'); ?></a>
+          <a href="<?php echo base_url("auth/login"); ?>" ><?php echo lang('btn_login'); ?></a>
         <?php } ?>
       </div>
     </div>
