4c9926803da218243cc61903212120668adfee4b
MestourBS <42637780+MestourBS@users.noreply.github.com>
2020-01-15 12:50:00 +0100

    Update MY_Exceptions to prevent recursive calls

--- application/core/MY_Exceptions.php
+++ application/core/MY_Exceptions.php
@@ -3,6 +3,12 @@
 class MY_Exceptions extends CI_Exceptions {
     public function show_error($heading, $message, $template = 'error_general', $status_code = 500)
     {
+        static $displayed = [
+            'header' => -1,
+            'login_bar' => -1,
+            'error' => -1,
+            'footer' => -1
+        ];
         $title = $heading;
 		$errors_templates_path = config_item('error_views_path');
 		if (empty($errors_templates_path))
@@ -35,12 +41,24 @@ class MY_Exceptions extends CI_Exceptions {
         }
         ob_start();
         if (!is_cli()) {
-            include($common_templates_path.'header.php');
-            include($common_templates_path.'login_bar.php');
+            $displayed['header']++;
+            if ($displayed['header'] <= 1) {
+                include($common_templates_path.'header.php');
+            }
+            $displayed['login_bar']++;
+            if ($displayed['login_bar'] <= 1) {
+                include($common_templates_path.'login_bar.php');
+            }
         }
-        include($errors_templates_path.$template.'.php');
+        $displayed['error']++;
+        if ($displayed['error'] <= 1) {
+            include($errors_templates_path.$template.'.php');
+        }
+        $displayed['footer']++;
         if (!is_cli()) {
-            include($common_templates_path.'footer.php');
+            if ($displayed['footer'] <= 1) {
+                include($common_templates_path.'footer.php');
+            }
         }
         $buffer = ob_get_contents();
         ob_end_clean();
