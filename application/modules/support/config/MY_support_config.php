57555a719f1480535b2405ab81afc83cd4d40df5
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2020-01-15 13:39:04 +0100

    Add support module

---
+++ application/modules/support/config/MY_support_config.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['github_repo'] = 'OrifInformatique/ci_packbase_hmvc';
+$config['github_username'] = 'orif-support';
+
