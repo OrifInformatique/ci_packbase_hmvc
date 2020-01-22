1220b9a3b5f171e720ffc06d760678cb38937112
MestourBS <42637780+MestourBS@users.noreply.github.com>
2020-01-22 13:38:02 +0100

    Prevent double view display and finish recursive prevention

--- application/core/MY_Exceptions.php
+++ application/core/MY_Exceptions.php
@@ -3,13 +3,16 @@
 class MY_Exceptions extends CI_Exceptions {
     public function show_error($heading, $message, $template = 'error_general', $status_code = 500)
     {
+        // If the function calls itself, this line cleans the stored buffer
+        ob_end_clean();
+        // If there is an error in one of the views,
+        // this prevents the view from being called again
         static $displayed = [
-            'header' => -1,
-            'login_bar' => -1,
-            'error' => -1,
-            'footer' => -1
+            'header' => 0,
+            'login_bar' => 0,
+            'error' => 0,
+            'footer' => 0
         ];
-        $title = $heading;
 		$errors_templates_path = config_item('error_views_path');
 		if (empty($errors_templates_path))
 		{
@@ -44,20 +47,27 @@ class MY_Exceptions extends CI_Exceptions {
             $displayed['header']++;
             if ($displayed['header'] <= 1) {
                 include($common_templates_path.'header.php');
+                $displayed['header']--;
             }
             $displayed['login_bar']++;
             if ($displayed['login_bar'] <= 1) {
                 include($common_templates_path.'login_bar.php');
+                $displayed['login_bar']--;
             }
         }
         $displayed['error']++;
         if ($displayed['error'] <= 1) {
             include($errors_templates_path.$template.'.php');
+            $displayed['error']--;
+        } else {
+            // Basic error display, as the view is not working
+            echo '==='.$heading.'==='.(is_cli() ? PHP_EOL : '<br />').$message;
         }
         if (!is_cli()) {
             $displayed['footer']++;
             if ($displayed['footer'] <= 1) {
                 include($common_templates_path.'footer.php');
+                $displayed['footer']--;
             }
         }
         $buffer = ob_get_contents();
