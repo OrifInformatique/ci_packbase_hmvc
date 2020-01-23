03cfd662e1d4a5e0395749d1821b71b0360fc488
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2020-01-23 08:58:51 +0100

    Add tests

---
+++ application/tests/controllers/Support_Test.php
@@ -0,0 +1,126 @@
+<?php
+// Load user controller for callback tests
+require_once(__DIR__.'/../../modules/support/controllers/Support.php');
+
+/**
+ * Class for tests for Support controller
+ * 
+ * @author      Orif, section informatique (ViDi, BuYa)
+ * @link        https://github.com/OrifInformatique/
+ * @copyright   Copyright (c) Orif (http://www.orif.ch)
+ */
+class Support_Test extends TestCase {
+
+    /**
+     * Called before a test
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->resetInstance();
+        // Required to load the correct url_helper
+        $this->CI->load->helper('url');
+    }
+
+    /***********
+     * PROVIDERS
+     ***********/
+    /**
+     * Provider for `test_fields_not_empty`
+     *
+     * @return array
+     */
+    public function provider_fields_not_empty() {
+        $this->resetInstance();
+
+        $this->CI->load->config('../modules/support/config/MY_support_config');
+        $this->CI->load->config('../modules/support/config/MY_support_token');
+        $this->CI->load->config('../modules/user/config/MY_user_config');
+
+        $data = [];    
+        $data['no_error'] = [
+            [
+                'issue_title' => 'Test',
+                'issue_body' => 'Test'
+            ],
+            FALSE
+        ];
+        $data['no_title'] = [
+            [
+                'issue_title' => '',
+                'issue_body' => 'Test'
+            ],
+            TRUE
+        ];
+        $data['no_body'] = [
+            [
+                'issue_title' => 'Test',
+                'issue_body' => ''
+            ],
+            TRUE
+        ];
+        $data['nothing'] = [
+            [
+                'issue_title' => '',
+                'issue_body' => ''
+            ],
+            TRUE
+        ];
+        return $data;
+    }
+
+    /*******
+     * TESTS
+     *******/
+    /**
+     * Test for `Support::index` without being logged
+     *
+     * @return void
+     */
+    public function test_index_not_logged()
+    {
+        $this->request('GET', 'support');
+
+        $this->assertRedirect('user/auth/login');
+    }
+
+    /**
+     * Test for `Support::form_report_problem`
+     * 
+     * @dataProvider provider_fields_not_empty
+     *
+     * @param array $post_params = Parameters to pass to $_POST
+     * @param boolean $error_expected = Whether an error is expected
+     * @return void
+     */
+    public function test_fields_not_empty(array $post_params, bool $error_expected){
+
+        self::_login_as_admin();
+        $output = $this->request('POST', 'support/form_report_problem', $post_params);
+
+        if ($error_expected) {
+            $this->assertNotEmpty(validation_errors());
+        }
+        else {
+            $this->CI->lang->load('../../modules/support/language/french/MY_support');
+            $this->assertContains($this->CI->lang->line('title_problem_submitted'), $output);
+        }
+    }
+
+    /**************
+     * MISC METHODS
+     **************/
+    /**
+     * Tricks the server to think there is an admin logged in
+     *
+     * @return void
+     */
+    private static function _login_as_admin()
+    {
+        $_SESSION['logged_in'] = TRUE;
+        // We can't know the current configuration for admin access, so we max it
+        $_SESSION['user_access'] = PHP_INT_MAX;
+        $_SESSION['user_id'] = 0;
+    }
+}
