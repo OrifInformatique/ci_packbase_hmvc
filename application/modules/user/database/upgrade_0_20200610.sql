4db9f1691a5787f00c9c63b95a8e3c8dc51467c9
Didier Viret <didier.viret@orif.ch>
2020-06-11 10:07:34 +0200

    authorize 100 chars for e-mail, rename sql scripts

--- application/modules/user/database/upgrade_0_20200610.sql
+++
@@ -1,4 +0,0 @@
---
--- Add email to `user`
---
-ALTER TABLE `user` ADD `email` VARCHAR(45) NULL DEFAULT NULL AFTER `password`;
--- application/modules/user/database/user_011.sql
+++ application/modules/user/database/user_011.sql
@@ -27,7 +27,7 @@ CREATE TABLE `user` (
   `fk_user_type` int(11) NOT NULL,
   `username` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `archive` tinyint(4) NOT NULL DEFAULT '0',
   `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
