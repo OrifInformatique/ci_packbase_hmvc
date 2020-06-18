043e42266a6a54ea2687c998106a7db7c37a345a
MeSa <42637780+MestourBS@users.noreply.github.com>
2020-06-18 16:25:00 +0200

    Add admin user
    Add a default "admin" user in the SQL script of user module

--- application/modules/user/database/user_011.sql
+++ application/modules/user/database/user_011.sql
@@ -32,6 +32,13 @@ CREATE TABLE `user` (
   `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `fk_user_type`, `username`, `password`) VALUES
+(1, 1, 'admin', '$2y$10$xD6VFLqS9PgCffufHzEJ9eW0ORtoA9w.W47EjHNDEkYaH/Cx8ElHW');
+
 -- --------------------------------------------------------
 
 --
