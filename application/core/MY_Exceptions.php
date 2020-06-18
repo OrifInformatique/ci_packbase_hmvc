2b663ad7fd9699210f1ca92ebd8100f9f439c6af
Didier Viret <didier.viret@orif.ch>
2020-06-18 16:04:50 +0200

    Rework MY_Exceptions

--- application/core/MY_Exceptions.php
+++ application/core/MY_Exceptions.php
@@ -3,75 +3,45 @@
 class MY_Exceptions extends CI_Exceptions {
     public function show_error($heading, $message, $template = 'error_general', $status_code = 500)
     {
-        // If the function calls itself, this line cleans the stored buffer
-        ob_end_clean();
-        // If there is an error in one of the views,
-        // this prevents the view from being called again
-        static $displayed = [
-            'header' => 0,
-            'login_bar' => 0,
-            'error' => 0,
-            'footer' => 0
-        ];
-		$errors_templates_path = config_item('error_views_path');
-		if (empty($errors_templates_path))
-		{
-			$errors_templates_path = VIEWPATH.'errors'.DIRECTORY_SEPARATOR;
-		}
+        // Common views path, in the common module
+        static $common_views_path = APPPATH.'modules'.DIRECTORY_SEPARATOR.'common'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR;
+        
+        // Header and footer views displayed before and after the error messages
+        static $header_views = ['header', 'login_bar'];
+        static $footer_views = ['footer'];
 
-		if (is_cli())
-		{
-			$message = "\t".(is_array($message) ? implode("\n\t", $message) : $message);
-			$template = 'cli'.DIRECTORY_SEPARATOR.$template;
-		}
-		else
-		{
-			set_status_header($status_code);
-			$message = '<p>'.(is_array($message) ? implode('</p><p>', $message) : $message).'</p>';
-			$template = 'html'.DIRECTORY_SEPARATOR.$template;
-        }
+        if (is_cli())
+        {
+            return parent::show_error($heading, $message, $template, $status_code);
 
-        // Default to application/views/common
-        $common_templates_path = VIEWPATH.'common'.DIRECTORY_SEPARATOR;
-        if (!file_exists($common_templates_path)) {
-            // Select the views in the common module
-            $common_templates_path = APPPATH.'modules'.DIRECTORY_SEPARATOR.'common'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR;
-        }
-
-		if (ob_get_level() > $this->ob_level + 1)
-		{
-			ob_end_flush();
-        }
-        ob_start();
-        if (!is_cli()) {
-            $displayed['header']++;
-            if ($displayed['header'] <= 1) {
-                include($common_templates_path.'header.php');
-                $displayed['header']--;
-            }
-            $displayed['login_bar']++;
-            if ($displayed['login_bar'] <= 1) {
-                include($common_templates_path.'login_bar.php');
-                $displayed['login_bar']--;
-            }
-        }
-        $displayed['error']++;
-        if ($displayed['error'] <= 1) {
-            include($errors_templates_path.$template.'.php');
-            $displayed['error']--;
         } else {
-            // Basic error display, as the view is not working
-            echo '==='.$heading.'==='.(is_cli() ? PHP_EOL : '<br />').$message;
-        }
-        if (!is_cli()) {
-            $displayed['footer']++;
-            if ($displayed['footer'] <= 1) {
-                include($common_templates_path.'footer.php');
-                $displayed['footer']--;
+            $message = parent::show_error($heading, $message, $template, $status_code);
+
+            if (ob_get_level() > $this->ob_level + 1)
+            {
+                ob_end_flush();
             }
+
+            ob_start();
+            // Include header views
+            foreach ($header_views as $view)
+            {
+                include($common_views_path.$view.'.php');
+            }
+
+            // Include error message
+            echo $message;
+
+            // Include footer views
+            foreach ($footer_views as $view)
+            {
+                include($common_views_path.$view.'.php');
+            }
+
+            $buffer = ob_get_contents();
+            ob_end_clean();
+
+            return $buffer;
         }
-        $buffer = ob_get_contents();
-        ob_end_clean();
-        return $buffer;
     }
 }
